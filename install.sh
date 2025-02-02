92b0f8bd7fbbad66d3768b9c46e2d944c46425ce
joaodanielcs <joaodanielcs@gmail.com>
2025-02-02 00:46:31 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade
+apt install sudo -y
+DUPLICATI_URL="https://github.com/duplicati/duplicati/releases/latest"
+DUPLICATI_PACKAGE=$(curl -s $DUPLICATI_URL | grep -oP '(?<=href=")[^"]+\.deb')
+wget "https://github.com/duplicati/duplicati/releases/download/$DUPLICATI_PACKAGE" -O duplicati_latest.deb
+sudo dpkg -i duplicati_latest.deb
+rm duplicati_latest.deb
+echo "Duplicati instalado com sucesso!"
