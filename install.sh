6dacd5b58a2eb2862a24f033681ad51ecbe7258b
joaodanielcs <joaodanielcs@gmail.com>
2025-02-17 23:10:24 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
+read -sp "Insira uma senha de acesso ao Duplicati: " pass
 apt update
 apt upgrade -y
 apt install sudo curl -y
-#DUPLICATI_URL="https://github.com/duplicati/duplicati/releases/latest"
-#DUPLICATI_PACKAGE=$(curl -s $DUPLICATI_URL | grep -oP '(?<=href=")[^"]+\.deb')
-#wget -q https://github.com/duplicati/duplicati/releases/download/$DUPLICATI_PACKAGE -O duplicati_latest.deb
 wget -q "https://updates.duplicati.com/stable/duplicati-2.1.0.4_stable_2025-01-31-linux-x64-gui.deb" -O duplicati_latest.deb
-sudo dpkg -i duplicati_latest.deb
+sudo dpkg -i duplicati_latest.deb ; apt-get -f install -y
+systemctl enable duplicati.service
+systemctl daemon-reload
+echo 'DAEMON_OPTS="--webservice-password='$pass' --webservice-interface=any --webservice-port=8200 --portable-mode"' | sudo tee /etc/default/duplicati
+systemctl start duplicati.service
+echo 'DAEMON_OPTS="--webservice-interface=any --webservice-port=8200 --portable-mode"' | sudo tee /etc/default/duplicati
 rm duplicati_latest.deb
-echo "Duplicati instalado com sucesso!"
+init 6
